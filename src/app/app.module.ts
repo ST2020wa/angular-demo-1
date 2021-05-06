df88ad39ce472de513b483a5ac1ff7051e9b0eea
ST Li <kidtime@126.com>
2021-05-06 23:58:13 +0800

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
 import {FormsModule} from '@angular/forms';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
