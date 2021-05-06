332e5509e1809eeef4ffca3096f1f341a7650df4
ST Li <kidtime@126.com>
2021-05-07 00:25:13 +0800

    update creating new component assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import {ServerComponent} from './server/server.component';
 import {FormsModule} from '@angular/forms';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
